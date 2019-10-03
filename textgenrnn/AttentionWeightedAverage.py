2ad1ddf86f3b14634235c7c3aae2a8e89977eb39
bartekkz <bartek.szmelczynski@gmail.com>
2019-10-03 22:43:49 +0200

    added build function to AttentionWeightedAverage

--- textgenrnn/AttentionWeightedAverage.py
+++ textgenrnn/AttentionWeightedAverage.py
@@ -5,7 +5,18 @@ from keras import initializers
 
 class AttentionWeightedAverage(Layer):
     def __init__(self, return_attention=False, **kwargs):
-        self.init = initializers.get("uniform")
-        self.supports_masking = True
-        self.return_attention = return_attention
-        super(AttentionWeightedAverage, self).__init__(**kwargs)
+      self.init = initializers.get("uniform")
+      self.supports_masking = True
+      self.return_attention = return_attention
+      super().__init__(**kwargs)
+
+    def build(self, input_shape):
+      self.input_spec = [InputSpec(ndim=3)]
+      assert len(input_shape) == 3
+
+      self.W = self.add_weight(shape=(input_shape[2], 1),
+                               name='{}_W'.format(self.name),
+                               initializer=self.init)
+      self.trainable_weights = [self.W]
+      super().build(input_shape)  
+    
