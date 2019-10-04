16cf30284414bb6ade4dbe41416a3106e1885ce8
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-10-04 21:21:46 +0200

    created call method for AttentionWeightedAverage

--- textgenrnn/AttentionWeightedAverage.py
+++ textgenrnn/AttentionWeightedAverage.py
@@ -8,4 +8,21 @@ class AttentionWeightedAverage(Layer):
         self.init = initializers.get("uniform")
         self.supports_masking = True
         self.return_attention = return_attention
-        super(AttentionWeightedAverage, self).__init__(**kwargs)
+        super().__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.input_spec = [InputSpec(ndim=3)]
+        assert len(input_shape) == 3
+
+        self.W = self.add_weight(shape=(input_shape[2], 1),
+                                name="{}_W".format(self.name),
+                                initializer=self.init)
+        self.trainable_weights = [self.W]
+        super().build(input_shape)
+
+    def call(self, x, mask=None):
+        logits = K.dot(x, self.W)
+        x_shape = K.shape(x)
+        logits = K.reshape(logits, (x_shape[0], x_shape[1]))
+        ai = K.exp(logits - K.max(logits, axis=-1, keepdims=True))
+        
