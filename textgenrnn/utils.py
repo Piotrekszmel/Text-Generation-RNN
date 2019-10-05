1e49159e7fb67a835e58dbab80c93b7b207c4f39
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-10-05 15:58:41 +0200

    created textgenrnn_sample()

--- textgenrnn/utils.py
+++ textgenrnn/utils.py
@@ -1,5 +1,17 @@
 from keras.layers import LSTM, Bidirectional
+from keras.callbacks import LearningRateScheduler, Callback
+from keras.models import Model, load_model
+from keras.preprocessing import sequence
+from keras.preprocessing.text import Tokenizer, text_to_word_sequence
 from keras import backend as K
+from sklearn.preprocessing import LabelBinarizer
+from random import shuffle
+from tqdm import trange
+import numpy as np
+import json
+import h5py
+import csv
+import re
 
 def new_rnn(cfg, layer_num):
     use_cudnnlstm = K.backend() == 'tensorflow' and len(K.tensorflow_backend._get_available_gpus()) > 0
@@ -24,3 +36,12 @@ def new_rnn(cfg, layer_num):
                     return_sequences=True,
                     recurrent_activation='sigmoid',
                     name='rnn_{}'.format(layer_num))
+
+
+
+def textgenrnn_sample(preds, temperature, interactive=False, top_n=3):
+    preds = np.asarray(preds).astype("float64")
+    if temperature is None or temperature == 0.0:
+      return np.argmax(preds)
+
+    preds = np.log(preds + K.epsilon()) / temperature
