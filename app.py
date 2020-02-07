c8c16864614231f0ca219bd1e36e8d78bfb3d1a5
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-02-07 21:48:05 +0100

    create weight scrollbar

--- app.py
+++ app.py
@@ -13,5 +13,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+app.route('/save_options', methods=["POST"])
+def save_options():
+    # TODO: change to post request
+    global temps, max_length
+    max_length = int(request.form["max_length"])
+    temps = [float(value) for value in request.form["temperatures"].split(',')]
+    return redirect(url_for("text_generation"))
+
+
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=5004)
